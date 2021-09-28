ee77ddc8c0932f3e6584c68c46880953dfd2c319
Ujwal Gautam <ujwal.gautam@cropdatatech.onmicrosoft.com>
2021-09-28 18:57:44 +0530

    init commit

---
+++ src/test/java/com/example/apigetway/exampleapigetway/ExampleapigetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigetway.exampleapigetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleapigetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
